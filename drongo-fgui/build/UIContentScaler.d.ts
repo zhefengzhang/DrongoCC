10b9de3e7f85c967160f8c374f4a905c5ec4a359
guojie.greg <653623647@qq.com>
2023-02-03 12:33:53 +0800

    忽略

--- drongo-fgui/build/UIContentScaler.d.ts
+++
@@ -1,7 +0,0 @@
-import { Size } from "cc";
-export declare class UIContentScaler {
-    static scaleFactor: number;
-    static scaleLevel: number;
-    static rootSize: Size;
-}
-export declare function updateScaler(): void;
--- drongo-fgui/build/gears/GearDisplay2.d.ts
+++
@@ -1,9 +0,0 @@
-import { GearBase } from "./GearBase";
-export declare class GearDisplay2 extends GearBase {
-    pages: string[];
-    condition: number;
-    private _visible;
-    protected init(): void;
-    apply(): void;
-    evaluate(connected: boolean): boolean;
-}
